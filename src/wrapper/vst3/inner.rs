83d934cd0a538f402c1a80bac71ca28039928d7c
Quant1um <blep@amee.ee>
2026-03-27 20:01:26 +0400

    fix background/gui task executor memory leaks

--- src/wrapper/vst3/inner.rs
+++ src/wrapper/vst3/inner.rs
@@ -332,17 +332,25 @@ impl<P: Vst3Plugin> WrapperInner<P> {
             .lock()
             .editor(AsyncExecutor {
                 execute_background: Arc::new({
-                    let wrapper = wrapper.clone();
-
+                    let wrapper = Arc::downgrade(&wrapper);
                     move |task| {
+                        let wrapper = match wrapper.upgrade() {
+                            Some(wrapper) => wrapper,
+                            None => return,
+                        };
+
                         let task_posted = wrapper.schedule_background(Task::PluginTask(task));
                         nih_debug_assert!(task_posted, "The task queue is full, dropping task...");
                     }
                 }),
                 execute_gui: Arc::new({
-                    let wrapper = wrapper.clone();
-
+                    let wrapper = Arc::downgrade(&wrapper);
                     move |task| {
+                        let wrapper = match wrapper.upgrade() {
+                            Some(wrapper) => wrapper,
+                            None => return,
+                        };
+
                         let task_posted = wrapper.schedule_gui(Task::PluginTask(task));
                         nih_debug_assert!(task_posted, "The task queue is full, dropping task...");
                     }
